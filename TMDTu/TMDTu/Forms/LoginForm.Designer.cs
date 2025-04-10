ab17141f8af91e21804a3ed0e28f7729474fdeff
thkd <thkd811@gmail.com>
2025-04-10 21:35:42 +0700

    Them form dang nhap

---
+++ TMDTu/TMDTu/Forms/LoginForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TMDTu.Forms
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlCaptcha = new System.Windows.Forms.Panel();
+            this.txtCaptcha = new System.Windows.Forms.TextBox();
+            this.lnkForgotPassword = new System.Windows.Forms.LinkLabel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsernameOrEmail = new System.Windows.Forms.TextBox();
+            this.pnlCaptcha.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlCaptcha
+            // 
+            this.pnlCaptcha.Controls.Add(this.txtCaptcha);
+            this.pnlCaptcha.Location = new System.Drawing.Point(197, 274);
+            this.pnlCaptcha.Name = "pnlCaptcha";
+            this.pnlCaptcha.Size = new System.Drawing.Size(200, 100);
+            this.pnlCaptcha.TabIndex = 9;
+            // 
+            // txtCaptcha
+            // 
+            this.txtCaptcha.Location = new System.Drawing.Point(48, 47);
+            this.txtCaptcha.Name = "txtCaptcha";
+            this.txtCaptcha.Size = new System.Drawing.Size(100, 22);
+            this.txtCaptcha.TabIndex = 0;
+            // 
+            // lnkForgotPassword
+            // 
+            this.lnkForgotPassword.AutoSize = true;
+            this.lnkForgotPassword.Location = new System.Drawing.Point(451, 249);
+            this.lnkForgotPassword.Name = "lnkForgotPassword";
+            this.lnkForgotPassword.Size = new System.Drawing.Size(96, 16);
+            this.lnkForgotPassword.TabIndex = 8;
+            this.lnkForgotPassword.TabStop = true;
+            this.lnkForgotPassword.Text = "Quên mật khẩu";
+            this.lnkForgotPassword.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkForgotPassword_LinkClicked);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(442, 320);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(111, 23);
+            this.btnLogin.TabIndex = 7;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.AccessibleDescription = "txtPassword";
+            this.txtPassword.Location = new System.Drawing.Point(401, 164);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(202, 22);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtUsernameOrEmail
+            // 
+            this.txtUsernameOrEmail.AccessibleDescription = "";
+            this.txtUsernameOrEmail.Location = new System.Drawing.Point(401, 77);
+            this.txtUsernameOrEmail.Name = "txtUsernameOrEmail";
+            this.txtUsernameOrEmail.Size = new System.Drawing.Size(202, 22);
+            this.txtUsernameOrEmail.TabIndex = 5;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlCaptcha);
+            this.Controls.Add(this.lnkForgotPassword);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsernameOrEmail);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.pnlCaptcha.ResumeLayout(false);
+            this.pnlCaptcha.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlCaptcha;
+        private System.Windows.Forms.TextBox txtCaptcha;
+        private System.Windows.Forms.LinkLabel lnkForgotPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsernameOrEmail;
+    }
+}

17a13ab917c7a6bb4f106c77554052577b036c3a
thkd <thkd811@gmail.com>
2025-04-10 22:39:07 +0700

    Them form thanh toan

---
+++ TMDTu/TMDTu/Forms/CartForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TMDTu.Forms
+{
+    partial class CartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlaceOrder = new System.Windows.Forms.Button();
+            this.dgvCart = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlaceOrder
+            // 
+            this.btnPlaceOrder.Location = new System.Drawing.Point(400, 392);
+            this.btnPlaceOrder.Name = "btnPlaceOrder";
+            this.btnPlaceOrder.Size = new System.Drawing.Size(75, 23);
+            this.btnPlaceOrder.TabIndex = 3;
+            this.btnPlaceOrder.Text = "Đặt hàng";
+            this.btnPlaceOrder.UseVisualStyleBackColor = true;
+            this.btnPlaceOrder.Click += new System.EventHandler(this.btnPlaceOrder_Click);
+            // 
+            // dgvCart
+            // 
+            this.dgvCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCart.Location = new System.Drawing.Point(159, 36);
+            this.dgvCart.Name = "dgvCart";
+            this.dgvCart.RowHeadersWidth = 51;
+            this.dgvCart.RowTemplate.Height = 24;
+            this.dgvCart.Size = new System.Drawing.Size(483, 275);
+            this.dgvCart.TabIndex = 2;
+            // 
+            // CartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPlaceOrder);
+            this.Controls.Add(this.dgvCart);
+            this.Name = "CartForm";
+            this.Text = "CartForm";
+            this.Load += new System.EventHandler(this.CartForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlaceOrder;
+        private System.Windows.Forms.DataGridView dgvCart;
+    }
+}
---
+++ TMDTu/TMDTu/Forms/CartForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TMDTu.Data;
+using TMDTu.Model;
+using TMDTu.Services;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace TMDTu.Forms
+{
+    public partial class CartForm : Form
+    {
+        private readonly User _currentUser;
+        private readonly List<(Product Product, int Quantity)> _cart;
+        private readonly OrderService _orderService;
+        public CartForm(User user, List<(Product Product, int Quantity)> cart)
+        {
+            InitializeComponent();
+            _currentUser = user;
+            _cart = cart;
+            _orderService = new OrderService(new AppDbContext());
+            LoadCart();
+        }
+        private void LoadCart()
+        {
+            dgvCart.DataSource = _cart.Select(item => new
+            {
+                ProductName = item.Product.Name,
+                Quantity = item.Quantity,
+                Price = item.Product.Price,
+                Total = item.Product.Price * item.Quantity
+            }).ToList();
+        }
+
+        private void btnPlaceOrder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_cart.Count == 0)
+                {
+                    MessageBox.Show("Giỏ hàng trống!");
+                    return;
+                }
+
+                _orderService.PlaceOrder(_currentUser, _cart);
+                MessageBox.Show("Đặt hàng thành công!");
+                _cart.Clear();
+                LoadCart();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi: {ex.Message}");
+            }
+
+        }
+
+        private void CartForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

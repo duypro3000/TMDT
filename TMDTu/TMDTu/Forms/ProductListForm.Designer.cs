0ff5cf59a14ec553fd6fbaf6a1aa0054190d3a52
thkd <thkd811@gmail.com>
2025-04-10 22:36:55 +0700

    Them form danh sach mat hang

---
+++ TMDTu/TMDTu/Forms/ProductListForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace TMDTu.Forms
+{
+    partial class ProductListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnViewCart = new System.Windows.Forms.Button();
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnViewCart
+            // 
+            this.btnViewCart.Location = new System.Drawing.Point(585, 414);
+            this.btnViewCart.Name = "btnViewCart";
+            this.btnViewCart.Size = new System.Drawing.Size(117, 23);
+            this.btnViewCart.TabIndex = 5;
+            this.btnViewCart.Text = "Xem giỏ hàng";
+            this.btnViewCart.UseVisualStyleBackColor = true;
+            this.btnViewCart.Click += new System.EventHandler(this.btnViewCart_Click);
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.Location = new System.Drawing.Point(336, 416);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(157, 23);
+            this.btnAddToCart.TabIndex = 4;
+            this.btnAddToCart.Text = "Thêm vào giỏ hàng";
+            this.btnAddToCart.UseVisualStyleBackColor = true;
+            this.btnAddToCart.Click += new System.EventHandler(this.btnAddToCart_Click);
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(78, 11);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.RowHeadersWidth = 51;
+            this.dgvProducts.RowTemplate.Height = 24;
+            this.dgvProducts.Size = new System.Drawing.Size(644, 378);
+            this.dgvProducts.TabIndex = 3;
+            // 
+            // ProductListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnViewCart);
+            this.Controls.Add(this.btnAddToCart);
+            this.Controls.Add(this.dgvProducts);
+            this.Name = "ProductListForm";
+            this.Text = "ProductListForm";
+            this.Load += new System.EventHandler(this.ProductListForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnViewCart;
+        private System.Windows.Forms.Button btnAddToCart;
+        private System.Windows.Forms.DataGridView dgvProducts;
+    }
+}
---
+++ TMDTu/TMDTu/Forms/ProductListForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TMDTu.Data;
+using TMDTu.Model;
+using TMDTu.Services;
+
+namespace TMDTu.Forms
+{
+    public partial class ProductListForm : Form
+    {
+        private readonly ProductService _productService;
+        private readonly User _currentUser;
+        private List<(Product Product, int Quantity)> _cart;
+        public ProductListForm(User user)
+        {
+            InitializeComponent();
+            _currentUser = user;
+            _productService = new ProductService(new AppDbContext());
+            _cart = new List<(Product, int)>();
+            LoadProducts();
+        }
+        private void LoadProducts()
+        {
+            var products = _productService.GetAllProducts();
+            dgvProducts.DataSource = products;
+        }
+        private void ProductListForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            if (dgvProducts.SelectedRows.Count > 0)
+            {
+                var selectedProduct = (Product)dgvProducts.SelectedRows[0].DataBoundItem;
+                var quantity = 1; // Có thể thêm TextBox để người dùng nhập số lượng
+                _cart.Add((selectedProduct, quantity));
+                MessageBox.Show($"Đã thêm {selectedProduct.Name} vào giỏ hàng!");
+            }
+        }
+
+        private void btnViewCart_Click(object sender, EventArgs e)
+        {
+            var cartForm = new CartForm(_currentUser, _cart);
+            cartForm.Show();
+            this.Hide();
+
+        }
+    }
+}

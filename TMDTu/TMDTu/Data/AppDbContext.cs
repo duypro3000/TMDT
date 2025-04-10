dcb6bd2c647c517ea8d7b674f8c7c6b229a8097b
thkd <thkd811@gmail.com>
2025-04-10 21:32:57 +0700

    Them AuthService cho dang nhap

---
+++ TMDTu/TMDTu/Data/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMDTu.Model;
+using System.Configuration;
+
+namespace TMDTu.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor EF Core cần
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Nếu bạn vẫn muốn khởi tạo bằng GetOptions() thì giữ thêm constructor này cũng được
+        public AppDbContext() : base(GetOptions())
+        {
+        }
+
+        // Hàm hỗ trợ tạo options khi dùng constructor không tham số
+        private static DbContextOptions<AppDbContext> GetOptions()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return optionsBuilder.Options;
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ TMDTu/TMDTu/Services/AuthService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMDTu.Data;
+using TMDTu.Model;
+
+namespace TMDTu.Services
+{
+    public class AuthService
+    {
+        private readonly AppDbContext _context;
+
+        public AuthService(AppDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public User Login(string usernameOrEmail, string password)
+        {
+            var user = _context.User
+                .FirstOrDefault(u => u.Username == usernameOrEmail || u.Email == usernameOrEmail);
+
+            if (user == null)
+            {
+                throw new Exception("Username hoặc password không đúng");
+            }
+
+            if (!user.Enable && user.TimeToEnable > DateTime.Now)
+            {
+                throw new Exception($"Tài khoản bị khóa đến {user.TimeToEnable}");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(password, user.Password))
+            {
+                user.LoginCount++;
+                if (user.LoginCount >= 5)
+                {
+                    user.Enable = false;
+                    user.TimeToEnable = DateTime.Now.AddMinutes(10);
+                    _context.SaveChanges();
+                    throw new Exception("Tài khoản bị khóa 10 phút");
+                }
+                _context.SaveChanges();
+                throw new Exception("Username hoặc password không đúng");
+            }
+
+            user.LoginCount = 0;
+            _context.SaveChanges();
+            return user;
+        }
+
+        public bool RequiresCaptcha(User user)
+        {
+            return user.LoginCount >= 3;
+        }
+    }
+}
